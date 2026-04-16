3c33edc041bc97f9db2fe715589c775ac0666f65
esthli <mariaesther2001.me@gmail.com>
2026-04-16 20:17:52 -0300

    Criei o formulario principal

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -1,10 +1,15 @@
 namespace Sistema_Locadora
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
