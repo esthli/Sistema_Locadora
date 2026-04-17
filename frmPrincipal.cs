97c2cdb6863b1dff2af42990e9e5206bf8975389
esthli <mariaesther2001.me@gmail.com>
2026-04-16 22:16:41 -0300

    Criei a aba Clientes e Filmes

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -11,5 +11,50 @@ namespace Sistema_Locadora
         {
 
         }
+
+        private void tsbtn_Clientes_Click(object sender, EventArgs e)
+        {
+            if (ts_Clientes.Visible == true)
+            {
+                gb_Clientes.Visible = false;
+                gb_Filmes.Visible = false;
+                ts_Clientes.Visible = false;
+            }
+
+            else
+            {
+                gb_Clientes.Visible = true;
+                ts_Clientes.Visible = true;
+            }
+
+            //  frm_Clientes frm_Clientes = new frm_Clientes(); 
+            //  frm_Clientes.ShowDialog();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            gb_Clientes.Visible = false;
+            ts_Clientes.Visible = false;
+            //  InitializeComponent();
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            
+            if (ts_Clientes.Visible == true)
+            {
+                gb_Filmes.Visible = false;
+                gb_Clientes.Visible = false;
+                ts_Clientes.Visible = false;
+            }
+            else
+            {
+                gb_Filmes.Visible = true;
+               ts_Clientes.Visible = true;
+            }
+    
+
+        }
     }
 }
